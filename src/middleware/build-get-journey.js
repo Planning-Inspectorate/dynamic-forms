6e1ca74d9b4b1001f5408789a5f61ce8b96097c8
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-12-16 12:27:11 +0000

    feat(dynamic-forms): include req in createJourney function for dynamic base URL

--- src/middleware/build-get-journey.js
+++ src/middleware/build-get-journey.js
@@ -1,14 +1,14 @@
 /**
- * @param {(response: import('../journey/journey-response.js').JourneyResponse) => import('../journey/journey.js').Journey} createJourney
+ * @param {(req: import('express').Request, journeyResponse: import('../journey/journey-response.js').JourneyResponse) => import('../journey/journey.js').Journey} createJourney
  * @returns {import('express').Handler}
  */
 export function buildGetJourney(createJourney) {
-	return (_, res, next) => {
+	return (req, res, next) => {
 		if (!('journeyId' in res.locals.journeyResponse)) {
 			throw new Error('no journey ID specified');
 		}
 		const { journeyId } = res.locals.journeyResponse;
-		const journey = createJourney(res.locals.journeyResponse);
+		const journey = createJourney(req, res.locals.journeyResponse);
 		if (journeyId !== journey.journeyId) {
 			throw new Error('journey ID mismatch');
 		}
