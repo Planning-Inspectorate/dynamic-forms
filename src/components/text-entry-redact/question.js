970f7f453592806b31ddccc5fc3e322e9b765613
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-03-10 15:30:01 +0000

    feat: add onlyShowRedactedValueForSummary option

--- src/components/text-entry-redact/question.js
+++ src/components/text-entry-redact/question.js
@@ -1,4 +1,5 @@
 import { Question } from '../../questions/question.js';
+
 /**
  * @typedef {import('../../questions/question.js').QuestionViewModel} QuestionViewModel
  * @typedef {import('../../journey/journey.js').Journey} Journey
@@ -30,9 +31,21 @@ export default class TextEntryRedactQuestion extends Question {
 	 * @param {string} [params.hint]
 	 * @param {TextEntryCheckbox} [params.textEntryCheckbox]
 	 * @param {string|undefined} [params.label] if defined this show as a label for the input and the question will just be a standard h1
+	 * @param {boolean} [params.onlyShowRedactedValueForSummary] whether to only show redacted value for summary
 	 * @param {Array.<import('../../validator/base-validator')>} [params.validators]
 	 */
-	constructor({ title, question, fieldName, url, hint, validators, html, textEntryCheckbox, label }) {
+	constructor({
+		title,
+		question,
+		fieldName,
+		url,
+		hint,
+		validators,
+		html,
+		textEntryCheckbox,
+		label,
+		onlyShowRedactedValueForSummary
+	}) {
 		super({
 			title,
 			viewFolder: 'text-entry-redact',
@@ -46,6 +59,7 @@ export default class TextEntryRedactQuestion extends Question {
 
 		this.textEntryCheckbox = textEntryCheckbox;
 		this.label = label;
+		this.onlyShowRedactedValueForSummary = onlyShowRedactedValueForSummary;
 	}
 
 	prepQuestionForRendering(section, journey, customViewData, payload) {
@@ -56,5 +70,16 @@ export default class TextEntryRedactQuestion extends Question {
 		viewModel.question.valueRedacted =
 			journey.response.answers[this.fieldName + 'Redacted'] || viewModel.question.value;
 		return viewModel;
+	}
+
+	formatAnswerForSummary(sectionSegment, journey, answer, capitals = true) {
+		const redacted = journey.response.answers[this.fieldName + 'Redacted'];
+		let toShow;
+		if (this.onlyShowRedactedValueForSummary) {
+			toShow = redacted;
+		} else {
+			toShow = redacted || answer;
+		}
+		return super.formatAnswerForSummary(sectionSegment, journey, toShow, capitals);
 	}
 }
