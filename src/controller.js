Planning-Inspectorate/crown-developments

fix(dynamic-forms): move save logic out of questions

packages/dynamic-forms/src/controller.js
@@ -154,26 +154,61 @@ export async function question(req, res) {
 }
 
 /**
- * @type {import('express').Handler}
+ * @typedef {Object} SaveParams
+ * @property {string} journeyId
+ * @property {string} referenceId
+ * @property {Object<string, any>} data
  */
-export async function save(req, res) {
-	//save the response
-	const { section, question } = req.params;
-	const { journey, journeyResponse } = res.locals;
 
-	const sectionObj = journey.getSection(section);
-	const questionObj = journey.getQuestionBySectionAndName(section, question);
+/**
+ * @param {(params: SaveParams) => Promise<void>} saveImpl
+ * @returns {import('express').Handler}
+ */
+export function buildSave(saveImpl) {
+	return async (req, res) => {
+		const { section, question } = req.params;
+		/** @type {import('./journey/journey.js').Journey} */
+		const journey = res.locals.journey;
+		/** @type {import('./journey/journey-response.js').JourneyResponse} */
+		const journeyResponse = res.locals.journeyResponse;
 
-	if (!questionObj || !sectionObj) {
-		return res.redirect(journey.taskListUrl);
-	}
+		const sectionObj = journey.getSection(section);
+		const questionObj = journey.getQuestionBySectionAndName(section, question);
 
-	try {
-		return await questionObj.saveAction(req, res, journey, sectionObj, journeyResponse);
-	} catch (err) {
-		const viewModel = questionObj.prepQuestionForRendering(sectionObj, journey, {
-			errorSummary: [{ text: err.toString(), href: '#' }]
-		});
-		return questionObj.renderAction(res, viewModel);
-	}
+		if (!questionObj || !sectionObj) {
+			return res.redirect(journey.taskListUrl);
+		}
+
+		try {
+			// check for validation errors
+			const errorViewModel = questionObj.checkForValidationErrors(req, section, journey);
+
+			if (errorViewModel) {
+				return questionObj.renderAction(res, errorViewModel);
+			}
+
+			// save
+			const data = await questionObj.getDataToSave(req, journeyResponse);
+
+			await saveImpl({
+				journeyId: journeyResponse.journeyId,
+				referenceId: journeyResponse.referenceId,
+				data
+			});
+
+			// check for saving errors
+			const saveViewModel = questionObj.checkForSavingErrors(req, section, journey);
+			if (saveViewModel) {
+				return questionObj.renderAction(res, saveViewModel);
+			}
+
+			// move to the next question
+			return questionObj.handleNextQuestion(res, journey, section.segment, questionObj.fieldName);
+		} catch (err) {
+			const viewModel = questionObj.prepQuestionForRendering(sectionObj, journey, {
+				errorSummary: [{ text: err.toString(), href: '#' }]
+			});
+			return questionObj.renderAction(res, viewModel);
+		}
+	};
 }

packages/dynamic-forms/src/controller.test.js
@@ -1,6 +1,6 @@
 import { describe, it, mock, beforeEach } from 'node:test';
 import assert from 'node:assert';
-import { list, question, save } from './controller.js';
+import { list, question, buildSave } from './controller.js';
 
 import { Journey } from './journey/journey.js';
 import { SECTION_STATUS } from './section.js';
@@ -151,6 +151,7 @@ const sections = [
 const journeyParams = {
 	sections,
 	journeyId: 'TEST',
+	referenceId: 'REF',
 	makeBaseUrl: () => `${mockBaseUrl}/${mockRef}`,
 	journeyTemplate: mockTemplateUrl,
 	listingPageViewPath: mockListingPath,
@@ -172,9 +173,13 @@ let mockSummaryListData;
 const sampleQuestionObj = {
 	fieldName: 'sampleFieldName',
 	renderAction: mock.fn(),
+	getDataToSave: mock.fn(),
+	checkForValidationErrors: mock.fn(),
+	checkForSavingErrors: mock.fn(),
 	prepQuestionForRendering: mock.fn(),
 	formatAnswerForSummary: mock.fn(() => [mockAnswer]),
-	viewFolder: 'sampleType'
+	viewFolder: 'sampleType',
+	handleNextQuestion: mock.fn()
 };
 
 const mockSection = {
@@ -293,9 +298,8 @@ describe('dynamic-form/controller', () => {
 	});
 
 	describe('save', () => {
-		it('should use question saveAction', async () => {
+		it('should use saveImpl', async () => {
 			const journeyId = 'has-questionnaire';
-			const sampleQuestionObjWithSaveAction = { ...sampleQuestionObj, saveAction: mock.fn() };
 
 			req.params = {
 				referenceId: mockRef,
@@ -304,6 +308,7 @@ describe('dynamic-form/controller', () => {
 			};
 
 			res.locals.journeyResponse = {
+				...journeyParams,
 				answers: {}
 			};
 
@@ -314,17 +319,18 @@ describe('dynamic-form/controller', () => {
 			};
 
 			mockJourney.getQuestionBySectionAndName = mock.fn();
-			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObjWithSaveAction);
+			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObj);
 
-			await save(req, res, journeyId);
+			const saveImpl = mock.fn();
+			await buildSave(saveImpl)(req, res, journeyId);
 
-			assert.strictEqual(sampleQuestionObjWithSaveAction.saveAction.mock.callCount(), 1);
-			assert.deepStrictEqual(sampleQuestionObjWithSaveAction.saveAction.mock.calls[0].arguments, [
-				req,
-				res,
-				mockJourney,
-				mockJourney.sections[0],
-				res.locals.journeyResponse
+			assert.strictEqual(saveImpl.mock.callCount(), 1);
+			assert.deepStrictEqual(saveImpl.mock.calls[0].arguments, [
+				{
+					journeyId: journeyParams.journeyId,
+					referenceId: journeyParams.referenceId,
+					data: undefined
+				}
 			]);
 		});
 
@@ -338,11 +344,6 @@ describe('dynamic-form/controller', () => {
 				renderAction: mock.fn()
 			};
 
-			mock.method(sampleQuestionObjWithActions, 'saveAction');
-			sampleQuestionObjWithActions.saveAction.mock.mockImplementation(() => {
-				throw new Error('Expected error message');
-			});
-
 			req.params = {
 				referenceId: mockRef,
 				section: mockJourney.sections[0].segment,
@@ -350,6 +351,7 @@ describe('dynamic-form/controller', () => {
 			};
 
 			res.locals.journeyResponse = {
+				...journeyParams,
 				answers: {}
 			};
 
@@ -362,21 +364,91 @@ describe('dynamic-form/controller', () => {
 			mockJourney.getQuestionBySectionAndName = mock.fn();
 			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObjWithActions);
 
-			await save(req, res, journeyId);
+			const saveImpl = mock.fn(() => {
+				throw new Error('Expected error message');
+			});
+			await buildSave(saveImpl)(req, res, journeyId);
 
-			assert.strictEqual(sampleQuestionObjWithActions.saveAction.mock.callCount(), 1);
-			assert.deepStrictEqual(sampleQuestionObjWithActions.saveAction.mock.calls[0].arguments, [
-				req,
-				res,
-				mockJourney,
-				mockJourney.sections[0],
-				res.locals.journeyResponse
+			assert.strictEqual(saveImpl.mock.callCount(), 1);
+			assert.deepStrictEqual(saveImpl.mock.calls[0].arguments, [
+				{
+					journeyId: journeyParams.journeyId,
+					referenceId: journeyParams.referenceId,
+					data: undefined
+				}
 			]);
 
 			assert.deepStrictEqual(sampleQuestionObjWithActions.renderAction.mock.calls[0].arguments, [
 				res,
 				expectedViewModel
 			]);
+		});
+
+		it('should handle validation errors', async () => {
+			const expectedErrors = {
+				errorViewModel: 'mocked-validation-error'
+			};
+			const journeyId = 'has-questionnaire';
+			const sampleQuestionObjWithActions = {
+				...sampleQuestionObj,
+				renderAction: mock.fn(),
+				checkForValidationErrors: mock.fn(() => expectedErrors)
+			};
+
+			req.params = {
+				referenceId: mockRef,
+				section: mockJourney.sections[0].segment,
+				question: mockJourney.sections[0].questions[0].fieldName
+			};
+
+			res.locals.journeyResponse = {
+				...journeyParams,
+				answers: {}
+			};
+
+			mockJourney.getQuestionBySectionAndName = mock.fn();
+			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObjWithActions);
+
+			const saveImpl = mock.fn();
+			await buildSave(saveImpl)(req, res, journeyId);
+
+			assert.strictEqual(saveImpl.mock.callCount(), 0);
+			assert.deepStrictEqual(sampleQuestionObjWithActions.renderAction.mock.calls[0].arguments, [res, expectedErrors]);
+		});
+
+		it('should handle saving', async () => {
+			res.redirect.mock.resetCalls();
+			const journeyId = 'has-questionnaire';
+			const expectedUrl = 'redirect-url';
+			const sampleQuestionObjWithActions = {
+				...sampleQuestionObj,
+				handleNextQuestion: mock.fn((res, journey) => {
+					res.redirect(journey.getNextQuestionUrl());
+				})
+			};
+
+			req.params = {
+				referenceId: mockRef,
+				section: mockJourney.sections[0].segment,
+				question: mockJourney.sections[0].questions[0].fieldName
+			};
+
+			res.locals.journeyResponse = {
+				...journeyParams,
+				answers: {}
+			};
+
+			mockJourney.getSection = mock.fn(() => ({}));
+			mockJourney.getQuestionBySectionAndName = mock.fn();
+			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObjWithActions);
+			mockJourney.getNextQuestionUrl = mock.fn(() => expectedUrl);
+
+			const saveImpl = mock.fn();
+			await buildSave(saveImpl)(req, res, journeyId);
+
+			assert.strictEqual(saveImpl.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.calls[0].arguments[0], expectedUrl);
 		});
 	});
 });

Planning-Inspectorate/dynamic-forms

src/controller.js
@@ -154,26 +154,64 @@ export async function question(req, res) {
 }
 
 /**
- * @type {import('express').Handler}
+ * @typedef {Object} SaveParams
+ * @property {import('express').Request} req
+ * @property {import('express').Response} res
+ * @property {string} journeyId
+ * @property {string} referenceId
+ * @property {Object<string, any>} data
  */
-export async function save(req, res) {
-	//save the response
-	const { section, question } = req.params;
-	const { journey, journeyResponse } = res.locals;
 
-	const sectionObj = journey.getSection(section);
-	const questionObj = journey.getQuestionBySectionAndName(section, question);
+/**
+ * @param {(params: SaveParams) => Promise<void>} saveData
+ * @returns {import('express').Handler}
+ */
+export function buildSave(saveData) {
+	return async (req, res) => {
+		const { section, question } = req.params;
+		/** @type {import('./journey/journey.js').Journey} */
+		const journey = res.locals.journey;
+		/** @type {import('./journey/journey-response.js').JourneyResponse} */
+		const journeyResponse = res.locals.journeyResponse;
 
-	if (!questionObj || !sectionObj) {
-		return res.redirect(journey.taskListUrl);
-	}
+		const sectionObj = journey.getSection(section);
+		const questionObj = journey.getQuestionBySectionAndName(section, question);
 
-	try {
-		return await questionObj.saveAction(req, res, journey, sectionObj, journeyResponse);
-	} catch (err) {
-		const viewModel = questionObj.prepQuestionForRendering(sectionObj, journey, {
-			errorSummary: [{ text: err.toString(), href: '#' }]
-		});
-		return questionObj.renderAction(res, viewModel);
-	}
+		if (!questionObj || !sectionObj) {
+			return res.redirect(journey.taskListUrl);
+		}
+
+		try {
+			// check for validation errors
+			const errorViewModel = questionObj.checkForValidationErrors(req, sectionObj, journey);
+			if (errorViewModel) {
+				return questionObj.renderAction(res, errorViewModel);
+			}
+
+			// save
+			const data = await questionObj.getDataToSave(req, journeyResponse);
+
+			await saveData({
+				req,
+				res,
+				journeyId: journeyResponse.journeyId,
+				referenceId: journeyResponse.referenceId,
+				data
+			});
+
+			// check for saving errors
+			const saveViewModel = questionObj.checkForSavingErrors(req, sectionObj, journey);
+			if (saveViewModel) {
+				return questionObj.renderAction(res, saveViewModel);
+			}
+
+			// move to the next question
+			return questionObj.handleNextQuestion(res, journey, sectionObj.segment, questionObj.fieldName);
+		} catch (err) {
+			const viewModel = questionObj.prepQuestionForRendering(sectionObj, journey, {
+				errorSummary: [{ text: err.toString(), href: '#' }]
+			});
+			return questionObj.renderAction(res, viewModel);
+		}
+	};
 }

src/controller.test.js
@@ -1,6 +1,6 @@
 import { describe, it, mock, beforeEach } from 'node:test';
 import assert from 'node:assert';
-import { list, question, save } from './controller.js';
+import { list, question, buildSave } from './controller.js';
 
 import { Journey } from './journey/journey.js';
 import { SECTION_STATUS } from './section.js';
@@ -151,6 +151,7 @@ const sections = [
 const journeyParams = {
 	sections,
 	journeyId: 'TEST',
+	referenceId: 'REF',
 	makeBaseUrl: () => `${mockBaseUrl}/${mockRef}`,
 	journeyTemplate: mockTemplateUrl,
 	listingPageViewPath: mockListingPath,
@@ -172,9 +173,13 @@ let mockSummaryListData;
 const sampleQuestionObj = {
 	fieldName: 'sampleFieldName',
 	renderAction: mock.fn(),
+	getDataToSave: mock.fn(),
+	checkForValidationErrors: mock.fn(),
+	checkForSavingErrors: mock.fn(),
 	prepQuestionForRendering: mock.fn(),
 	formatAnswerForSummary: mock.fn(() => [mockAnswer]),
-	viewFolder: 'sampleType'
+	viewFolder: 'sampleType',
+	handleNextQuestion: mock.fn()
 };
 
 const mockSection = {
@@ -293,9 +298,8 @@ describe('dynamic-form/controller', () => {
 	});
 
 	describe('save', () => {
-		it('should use question saveAction', async () => {
+		it('should use saveData', async () => {
 			const journeyId = 'has-questionnaire';
-			const sampleQuestionObjWithSaveAction = { ...sampleQuestionObj, saveAction: mock.fn() };
 
 			req.params = {
 				referenceId: mockRef,
@@ -304,6 +308,7 @@ describe('dynamic-form/controller', () => {
 			};
 
 			res.locals.journeyResponse = {
+				...journeyParams,
 				answers: {}
 			};
 
@@ -314,18 +319,27 @@ describe('dynamic-form/controller', () => {
 			};
 
 			mockJourney.getQuestionBySectionAndName = mock.fn();
-			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObjWithSaveAction);
+			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObj);
 
-			await save(req, res, journeyId);
+			const saveData = mock.fn();
+			await buildSave(saveData)(req, res, journeyId);
 
-			assert.strictEqual(sampleQuestionObjWithSaveAction.saveAction.mock.callCount(), 1);
-			assert.deepStrictEqual(sampleQuestionObjWithSaveAction.saveAction.mock.calls[0].arguments, [
-				req,
-				res,
-				mockJourney,
-				mockJourney.sections[0],
-				res.locals.journeyResponse
+			assert.strictEqual(saveData.mock.callCount(), 1);
+			assert.deepStrictEqual(saveData.mock.calls[0].arguments, [
+				{
+					req,
+					res,
+					journeyId: journeyParams.journeyId,
+					referenceId: journeyParams.referenceId,
+					data: undefined
+				}
 			]);
+			// check other function calls
+			const mocks = [sampleQuestionObj.checkForValidationErrors.mock, sampleQuestionObj.checkForSavingErrors.mock];
+			for (const mockFn of mocks) {
+				assert.strictEqual(mockFn.callCount(), 1);
+				assert.deepStrictEqual(mockFn.calls[0].arguments, [req, sections[0], mockJourney]);
+			}
 		});
 
 		it('should handle error', async () => {
@@ -338,11 +352,6 @@ describe('dynamic-form/controller', () => {
 				renderAction: mock.fn()
 			};
 
-			mock.method(sampleQuestionObjWithActions, 'saveAction');
-			sampleQuestionObjWithActions.saveAction.mock.mockImplementation(() => {
-				throw new Error('Expected error message');
-			});
-
 			req.params = {
 				referenceId: mockRef,
 				section: mockJourney.sections[0].segment,
@@ -350,6 +359,7 @@ describe('dynamic-form/controller', () => {
 			};
 
 			res.locals.journeyResponse = {
+				...journeyParams,
 				answers: {}
 			};
 
@@ -362,21 +372,93 @@ describe('dynamic-form/controller', () => {
 			mockJourney.getQuestionBySectionAndName = mock.fn();
 			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObjWithActions);
 
-			await save(req, res, journeyId);
+			const saveData = mock.fn(() => {
+				throw new Error('Expected error message');
+			});
+			await buildSave(saveData)(req, res, journeyId);
 
-			assert.strictEqual(sampleQuestionObjWithActions.saveAction.mock.callCount(), 1);
-			assert.deepStrictEqual(sampleQuestionObjWithActions.saveAction.mock.calls[0].arguments, [
-				req,
-				res,
-				mockJourney,
-				mockJourney.sections[0],
-				res.locals.journeyResponse
+			assert.strictEqual(saveData.mock.callCount(), 1);
+			assert.deepStrictEqual(saveData.mock.calls[0].arguments, [
+				{
+					req,
+					res,
+					journeyId: journeyParams.journeyId,
+					referenceId: journeyParams.referenceId,
+					data: undefined
+				}
 			]);
 
 			assert.deepStrictEqual(sampleQuestionObjWithActions.renderAction.mock.calls[0].arguments, [
 				res,
 				expectedViewModel
 			]);
+		});
+
+		it('should handle validation errors', async () => {
+			const expectedErrors = {
+				errorViewModel: 'mocked-validation-error'
+			};
+			const journeyId = 'has-questionnaire';
+			const sampleQuestionObjWithActions = {
+				...sampleQuestionObj,
+				renderAction: mock.fn(),
+				checkForValidationErrors: mock.fn(() => expectedErrors)
+			};
+
+			req.params = {
+				referenceId: mockRef,
+				section: mockJourney.sections[0].segment,
+				question: mockJourney.sections[0].questions[0].fieldName
+			};
+
+			res.locals.journeyResponse = {
+				...journeyParams,
+				answers: {}
+			};
+
+			mockJourney.getQuestionBySectionAndName = mock.fn();
+			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObjWithActions);
+
+			const saveData = mock.fn();
+			await buildSave(saveData)(req, res, journeyId);
+
+			assert.strictEqual(saveData.mock.callCount(), 0);
+			assert.deepStrictEqual(sampleQuestionObjWithActions.renderAction.mock.calls[0].arguments, [res, expectedErrors]);
+		});
+
+		it('should handle saving', async () => {
+			res.redirect.mock.resetCalls();
+			const journeyId = 'has-questionnaire';
+			const expectedUrl = 'redirect-url';
+			const sampleQuestionObjWithActions = {
+				...sampleQuestionObj,
+				handleNextQuestion: mock.fn((res, journey) => {
+					res.redirect(journey.getNextQuestionUrl());
+				})
+			};
+
+			req.params = {
+				referenceId: mockRef,
+				section: mockJourney.sections[0].segment,
+				question: mockJourney.sections[0].questions[0].fieldName
+			};
+
+			res.locals.journeyResponse = {
+				...journeyParams,
+				answers: {}
+			};
+
+			mockJourney.getSection = mock.fn(() => ({}));
+			mockJourney.getQuestionBySectionAndName = mock.fn();
+			mockJourney.getQuestionBySectionAndName.mock.mockImplementationOnce(() => sampleQuestionObjWithActions);
+			mockJourney.getNextQuestionUrl = mock.fn(() => expectedUrl);
+
+			const saveData = mock.fn();
+			await buildSave(saveData)(req, res, journeyId);
+
+			assert.strictEqual(saveData.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.callCount(), 1);
+			assert.strictEqual(res.redirect.mock.calls[0].arguments[0], expectedUrl);
 		});
 	});
 });