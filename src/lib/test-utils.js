7079fa55f03a231be06f19bc7d765b1b11fc3f4d
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-07-02 11:48:05 +0100

    feat: add suggestions UI option to text-entry-redact

--- src/lib/test-utils.js
+++ src/lib/test-utils.js
@@ -1,4 +1,11 @@
 import { mock } from 'node:test';
+import { createRequire } from 'node:module';
+import nunjucks from 'nunjucks';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * @returns {import('pino').BaseLogger}
@@ -35,3 +42,14 @@ export const mockRes = () => {
 
 	return res;
 };
+
+export function configureNunjucks() {
+	const require = createRequire(import.meta.url);
+	const paths = [
+		// get the path to the govuk-frontend folder, in node_modules, using the node require resolution
+		path.resolve(require.resolve('govuk-frontend'), '../..'),
+		// path to src folder
+		path.join(__dirname, '..')
+	];
+	return nunjucks.configure(paths);
+}
