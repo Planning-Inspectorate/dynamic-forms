d4f45a2f559fd633f64ae4cf014020dcf8c474b9
Rob Broadhead <rob.broadhead.30@planninginspectorate.gov.uk>
2025-11-04 09:50:26 +0000

    feat: add a same answer validator and test

---
+++ src/validator/same-answer-validator.js
@@ -0,0 +1,32 @@
+import { body } from 'express-validator';
+import BaseValidator from './base-validator.js';
+
+/**
+ * Universal validator to ensure the answer to the current question is not the same as another question's answer
+ */
+export default class SameAnswerValidator extends BaseValidator {
+	/**
+	 * @param {string[]} fieldNamesToCompare - field name to compare against
+	 * @param {string} [errorMessage]
+	 */
+
+	constructor(fieldNamesToCompare, errorMessage) {
+		super();
+		this.fieldNamesToCompare = fieldNamesToCompare || [];
+		this.errorMessage = errorMessage || 'This answer cannot be the same as another answer';
+	}
+
+	/**
+	 * validates the questionToCompare and compares it to the answer in the journeyResponse
+	 * @param questionObj
+	 */
+	validate(questionObj) {
+		return body(questionObj.fieldName).custom((value, { req }) => {
+			const answers = req?.res?.locals?.journeyResponse?.answers || {};
+			if (this.fieldNamesToCompare.some((field) => value === answers[field])) {
+				throw new Error(this.errorMessage);
+			}
+			return true;
+		});
+	}
+}
