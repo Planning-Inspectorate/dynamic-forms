f4d29b96191cf56bc01df3155023a0b2e7dfae8a
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-11-21 10:38:24 +0000

    feat(lib): export postcode validator so classes extending can use it

--- src/validator/address-validator.js
+++ src/validator/address-validator.js
@@ -2,7 +2,7 @@ import { body } from 'express-validator';
 
 import BaseValidator from './base-validator.js';
 
-const validatePostcode = (postcode, errorMessage = 'Enter a valid postcode') => {
+export const validatePostcode = (postcode, errorMessage = 'Enter a valid postcode') => {
 	const pattern =
 		/([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/;
 	const result = pattern.exec(postcode);
