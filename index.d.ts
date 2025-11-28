6476569e92b4fd41760be5cb49adc38912aaef78
Jan Phillips <jan.phillips.82@planninginspectorate.gov.uk>
2025-11-28 16:40:29 +0000

    feat/first-attempt-exporting-types

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+// Export all component types
+export * from './src/components/index.js';
+export * from './src/journey/index.js';
+export * from './src/question/index.js';
+export * from './src/section/index.js';
+export * from './src/response/index.js';
+
+// Export utility types
+export * from './src/components/utils/question-has-answer.js';
+
+// Main classes
+export { Journey } from './src/journey/journey.js';
+export { Section } from './src/section/section.js';
+export { Question } from './src/question/question.js';
+export { Response } from './src/response/response.js';
