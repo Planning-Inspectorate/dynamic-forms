442a1b2658b670afad547128c0841b96e2dc89c3
Jan Phillips <jan.phillips.82@planninginspectorate.gov.uk>
2025-12-16 15:35:07 +0000

    only exporting one validate

--- types/src/validator/validator.d.ts
+++ types/src/validator/validator.d.ts
@@ -1,4 +1,4 @@
 export function buildValidateBody(questions: any): (req: any, res: any, next: any) => Promise<any>;
 export default validate;
 /** @type {import('express').Handler} */
-declare const validate: any;
+// declare const validate: any;
