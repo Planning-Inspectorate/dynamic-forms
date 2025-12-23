404e3042dd171638bb1d1fd586d919b3bd8a1866
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-12-23 09:20:44 +0000

    chore(lib): add randomUUID with mocking utility

--- src/lib/uuid.test.js
+++ src/lib/uuid.test.js
@@ -1,4 +1,6 @@
 import { Uuid } from '#src/lib/uuid.js';
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
 
 /**
  *
@@ -11,3 +13,12 @@ export function mockRandomUUID(ctx, uuid = '00000000-0000-0000-0000-000000000000
 	// restore after the tests
 	ctx.after(() => (Uuid.randomUUID = original));
 }
+
+describe('uuid', () => {
+	it('should return a valid v4 UUID', () => {
+		assert.match(
+			Uuid.randomUUID(),
+			/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/
+		);
+	});
+});
