f21add482a8eab07f114c9a750b52da3c1f9d0f5
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-02-24 15:17:31 +0000

    chore: sort COMPONENT_TYPES

--- src/components/utils/component-types.js
+++ src/components/utils/component-types.js
@@ -1,18 +1,18 @@
 export const COMPONENT_TYPES = Object.freeze({
-	CHECKBOX: 'checkbox',
+	ADDRESS: 'site-address',
 	BOOLEAN: 'boolean',
-	RADIO: 'radio',
+	CHECKBOX: 'checkbox',
 	DATE: 'date',
 	DATE_PERIOD: 'date-period',
 	DATE_TIME: 'date-time',
-	TEXT_ENTRY: 'text-entry',
-	TEXT_ENTRY_REDACT: 'text-entry-redact',
-	SELECT: 'select',
-	SINGLE_LINE_INPUT: 'single-line-input',
+	EMAIL: 'email',
+	MANAGE_LIST: 'manage-list',
 	MULTI_FIELD_INPUT: 'multi-field-input',
 	NUMBER: 'number',
-	ADDRESS: 'site-address',
-	UNIT_OPTION: 'unit-option',
-	EMAIL: 'email',
-	MANAGE_LIST: 'manage-list'
+	RADIO: 'radio',
+	SELECT: 'select',
+	SINGLE_LINE_INPUT: 'single-line-input',
+	TEXT_ENTRY: 'text-entry',
+	TEXT_ENTRY_REDACT: 'text-entry-redact',
+	UNIT_OPTION: 'unit-option'
 });
