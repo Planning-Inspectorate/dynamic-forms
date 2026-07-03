bd3704938991d2dd2ddd9fd77c905dec9d9aa3c1
Steve Brett <Steve.Brett.04@planninginspectorate.gov.uk>
2026-07-03 08:52:57 +0100

    feat(validator): add abstract validate method to BaseValidator

--- src/validator/base-validator.js
+++ src/validator/base-validator.js
@@ -13,6 +13,19 @@ export class BaseValidator {
 			throw new Error("Abstract classes can't be instantiated.");
 		}
 	}
+
+	/**
+	 * Validates response body against field validators.
+	 * Subclasses must override this method.
+	 * @abstract
+	 * @param {import('../questions/question-props.js').QuestionProps} questionObj - The question object containing the fieldName to validate.
+	 * @param {import('../journey/journey-response.js').JourneyResponse} [journeyResponse] - The current journey response (optional).
+	 * @returns {import('express-validator').ValidationChain | import('express-validator').ValidationChain[]}
+	 */
+	// eslint-disable-next-line no-unused-vars
+	validate(questionObj, journeyResponse) {
+		throw new Error('validate method must be implemented by subclass');
+	}
 }
 
 export default BaseValidator;
