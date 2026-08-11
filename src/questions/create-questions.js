011b2fcbccc11d024d3f1e4f40fcc8252114c272
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-08-11 10:30:42 +0100

    fix(types): preserve keys in return type of createQuestions

--- src/questions/create-questions.js
+++ src/questions/create-questions.js
@@ -1,10 +1,11 @@
 /**
+ * @template {string} K Question keys in questionPropsRecord
  * @template {import('./question-props.js').BaseQuestionProps} T
- * @param {{[questionName: string]: T}} questionPropsRecord
+ * @param {{[questionName in K]: T}} questionPropsRecord
  * @param {Record<string, import('./question-types.js').QuestionClass>} questionClasses
  * @param {{[questionType: string]: Record<string, Function>}} questionMethodOverrides
  * @param {{notStartedText?: string, continueButtonText?: string, changeActionText?: string, answerActionText?: string}} [textOverrides] - customise question text
- * @returns {Record<string, InstanceType<import('./question-types.js').QuestionClass>>}
+ * @returns {{[questionName in K]: InstanceType<import('./question-types.js').QuestionClass>}} Returns the same question keys that were passed in with each value being an instantiated question
  */
 export function createQuestions(questionPropsRecord, questionClasses, questionMethodOverrides, textOverrides) {
 	return Object.fromEntries(
